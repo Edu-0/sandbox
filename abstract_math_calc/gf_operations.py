5c9996325fa3a6cf4d83c206d1982f676d1d6370
Edu-0 <164441707+Edu-0@users.noreply.github.com>
2026-02-25 20:45:51 -0300

    feature: Added multiplication GF(2) without reduction

--- abstract_math_calc/gf_operations.py
+++ abstract_math_calc/gf_operations.py
@@ -3,8 +3,7 @@ def gf_mul(f, g, m):
     for i in range(8):
         if (g >> i) & 1:  # If the i th value of g is 1 (Dislocation done through shift right, from MSB to LSB)
             temp = f  # f(x) will be used more times, so I save on a temp
-            for _ in range(
-                    i):  # Depending on the position of the 1 in the binary number, it'll do shift lefts for the same number as position
+            for _ in range(i):  # Depending on the position of the 1 in the binary number, it'll do shift lefts for the same number as position
                 of = bin((temp & 0x80) >> 7)[2:]  # Verifying if there will be an overflow each time
                 temp = (temp << 1) & 0xFF  # I cap the maximum size to 8 bits and shift left
                 if of == "1":
@@ -13,6 +12,16 @@ def gf_mul(f, g, m):
             result ^= temp  # Sum to the final result with an XOR
     return bin(result)
 
+def gf2_mul(f, g):
+    result = 0
+    for i in range(8):
+        if (g >> i) & 1:  # If the i th value of g is 1 (Dislocation done through shift right, from MSB to LSB)
+            temp = f  # f(x) will be used more times, so I save on a temp
+            for _ in range(i):  # Depending on the position of the 1 in the binary number, it'll do shift lefts for the same number as position
+                temp = temp << 1  # I cap the maximum size to 8 bits and shift left
+
+            result ^= temp  # Sum to the final result with an XOR
+    return bin(result)
 
 f = 0b01010111
 g = 0b10000011
@@ -26,7 +35,13 @@ print(f"Sum of {bin(f)} + {bin(g)} = {bin(sum_bin)}")
 
 # * -> Multiplication
 
-print(f"Multiplication of {bin(f)} + {bin(g)} = {gf_mul(f, g, m)}")
+print(f"Multiplication of {bin(f)} * {bin(g)} = {gf_mul(f, g, m)}")
+
+# GF(2) Multiplication (No reduction, needed for multiplicative inverse)
+
+x = 0b10000011 # (x^7 + x + 1)
+y = 0b10000000 # (x^7)
+print(f"Multiplication in GF(2) of {bin(x)} * {bin(y)} = {gf2_mul(x, y)}") # (x^7 + x + 1)*(x^7) = x^14 + x^8 + x^7
 
 # Polynomial division
 
