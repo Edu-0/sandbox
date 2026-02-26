a920ac5b88747779d6065f3337bf55418692af8a
Edu-0 <164441707+Edu-0@users.noreply.github.com>
2026-02-25 21:04:27 -0300

    feature: Working function to calculate degree of a polynomial, with tests

--- abstract_math_calc/gf_operations.py
+++ abstract_math_calc/gf_operations.py
@@ -10,18 +10,18 @@ def gf_mul(f, g, m):
                     temp ^= m  # If it leads to an OF, reduce
 
             result ^= temp  # Sum to the final result with an XOR
-    return bin(result)
+    return result
 
-def gf2_mul(f, g):
+def gf2_mul(f, g): # This function had 0xFF, 0x80 and Overflow because here it doesn't matter if it goes beyond x^8
     result = 0
     for i in range(8):
-        if (g >> i) & 1:  # If the i th value of g is 1 (Dislocation done through shift right, from MSB to LSB)
-            temp = f  # f(x) will be used more times, so I save on a temp
-            for _ in range(i):  # Depending on the position of the 1 in the binary number, it'll do shift lefts for the same number as position
-                temp = temp << 1  # I cap the maximum size to 8 bits and shift left
+        if (g >> i) & 1:
+            temp = f
+            for _ in range(i):
+                temp = temp << 1
 
-            result ^= temp  # Sum to the final result with an XOR
-    return bin(result)
+            result ^= temp
+    return result
 
 f = 0b01010111
 g = 0b10000011
@@ -35,13 +35,13 @@ print(f"Sum of {bin(f)} + {bin(g)} = {bin(sum_bin)}")
 
 # * -> Multiplication
 
-print(f"Multiplication of {bin(f)} * {bin(g)} = {gf_mul(f, g, m)}")
+print(f"Multiplication of {bin(f)} * {bin(g)} = {bin(gf_mul(f, g, m))}")
 
 # GF(2) Multiplication (No reduction, needed for multiplicative inverse)
 
 x = 0b10000011 # (x^7 + x + 1)
 y = 0b10000000 # (x^7)
-print(f"Multiplication in GF(2) of {bin(x)} * {bin(y)} = {gf2_mul(x, y)}") # (x^7 + x + 1)*(x^7) = x^14 + x^8 + x^7
+print(f"Multiplication in GF(2) of {bin(x)} * {bin(y)} = {bin(gf2_mul(x, y))}") # (x^7 + x + 1)*(x^7) = x^14 + x^8 + x^7
 
 # Polynomial division
 
@@ -62,9 +62,19 @@ r = a, to keep the value of a intact, as it'll be used
 """
 
 
-def deg(a, b):
-    pass
+# Takes the highest degree of a polynom
+def deg(b):
+    for i in range(len(bin(b)[2:]), -1, -1):
+        if (b >> i) & 1:
+            return i
+    return 0
 
+print(deg(m)) # x^4 + x^3 + x + 1 -> Degree of 4
+print(deg(gf2_mul(x, y))) # x^14 + x^8 + x^7 -> degree of 14
+print(deg(x)) # x^7 + x + 1 -> degree of 7
+print(deg(f)) # 0b01010111 -> degree of 6
+print(deg(0b00000000)) # degree 0
+print(deg(0b11111111)) # degree 7
 
 def gf2_div(a, b):
     pass
