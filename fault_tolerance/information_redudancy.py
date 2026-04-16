4912b1c9bca992a18bd863d59cc73db801410df8
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-04-16 17:13:11 -0300

    feat: parity bits calculated

--- fault_tolerance/information_redudancy.py
+++ fault_tolerance/information_redudancy.py
@@ -25,13 +25,40 @@ print(cd_arr)
 
 # Filling codeword array
 data_i = 0
-for i in range(0, 73):
+for i in range(1, 73):
     if not is_pow_of_2(i):
-        cd_arr[i] = arr[data_i]
+        cd_arr[i-1] = arr[data_i]
         data_i += 1
-        if data_i == 64: break
+        if data_i == 64:
+            break
 
 print(cd_arr)
 
+# Calculating parity bits
+
+# It needs to XOR every number which:
+# -> Isn't a power of 2 / parity position
+# -> The i th bit of the position number is = 1.
+
+for k in range(0, 7):
+    p = pow(2, k)
+    parity_bit = 0
+    for i in range(1, 73):
+        if i == p:
+            continue
+        if ((i >> k) & 1) == 1:
+            parity_bit ^= cd_arr[i-1]
+    cd_arr[p-1] = parity_bit
+
+print(f"Codeword Array: {cd_arr}")
+
+# Calculating Overall Parity:
+overall = 0
+for i in range(72):
+    overall ^= cd_arr[i]
+cd_arr[71] = overall
+
+print(f"Codeword Array Finished: \n{cd_arr}")
+
 for index in np.ndindex(cd_arr.shape):
-    print(index, cd_arr[index])
+    print(index[0]+1, cd_arr[index])
