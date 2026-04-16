eab87070ad5b358188aa18793b26eb150acf87c8
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-04-16 18:04:27 -0300

    feat: create parity check matriz is now working and it's documented to explain how it's done

--- fault_tolerance/information_redudancy.py
+++ fault_tolerance/information_redudancy.py
@@ -13,6 +13,8 @@ DATAWORD_SIZE = 64
 CODEWORD_SIZE = 72
 PARITY_BITS = 7
 
+H = np.zeros((PARITY_BITS, CODEWORD_SIZE), dtype="u1")
+
 def is_pow_of_2(n):
     return n > 0 and (n & (n-1)) == 0
 
@@ -50,12 +52,34 @@ def data_to_codeword(arr):
     # Filling codeword array
     data_i = 0
     for i in range(1, CODEWORD_SIZE+1):
-        if not is_pow_of_2(i):
+        if not is_pow_of_2(i): # Skips powers of 2
             cd_arr[i - 1] = arr[data_i]
             data_i += 1
             if data_i == 64:
                 break
     return cd_arr
+
+
+def create_parity_check_matrix():
+    for j in range(1, CODEWORD_SIZE+1):
+        # For j = 5 (0000101) and 7 parity bits, I separate 1 bit for each K. Binary = [1,0,1,0,0,0,0]
+        # Does it 72 times as there are 72 columns and all need to receive the binary version
+        binary = [(j >> k) & 1 for k in range(PARITY_BITS)]
+        # Each column of the matrix receives the binary of j, as it has the same height as number os lines
+        H[:, j-1] = binary
+
+        # Visual explaining extracted from a print:
+        # [[1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0]
+        #  [0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1 0]
+        #  [0 0 0 1 1 1 1 0 0 0 0 1 1 1 1 0 0 0 0 1 1 1 1 0 0 0 0 1 1 1 1 0 0 0 0 1 1 1 1 0 0 0 0 1 1 1 1 0 0 0 0 1 1 1 1 0 0 0 0 1 1 1 1 0 0 0 0 1 1 1 1 0]
+        #  [0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 1]
+        #  [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0]
+        #  [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0]
+        #  [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1]]
+        # As it can be seen, the first column is = 1, 1000000 | The last column: 1001000, which is equal to 72
+
+def ecc():
+    pass
 
 
 if __name__ == "__main__":
@@ -70,3 +94,6 @@ if __name__ == "__main__":
 
     for index in np.ndindex(codeword.shape):
         print(index[0] + 1, codeword[index])
+
+    create_parity_check_matrix()
+    print(H)
