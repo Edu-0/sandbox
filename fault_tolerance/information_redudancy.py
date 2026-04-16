43fa0097ff3997cf150c504fbfc30f78252b25f4
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-04-16 17:28:47 -0300

    refactor: organized the code to functions to keep it cleaner and less confusing

--- fault_tolerance/information_redudancy.py
+++ fault_tolerance/information_redudancy.py
@@ -9,56 +9,61 @@
 import secrets
 import numpy as np
 
+
 def is_pow_of_2(n):
     return n > 0 and (n & (n-1)) == 0
 
-# Generating the first 64 bits that will be used
-# bin_str = format(secrets.randbits(64), '064b')
-# arr = np.fromiter(bin_str, dtype="u1")
 
-arr = np.ones(64, dtype="u1")
-print(arr)
+def calculate_parity_bits(arr):
+    # Calculating parity bits
 
-# Generating the codeword array
-cd_arr = np.zeros(72, dtype="u1")
-print(cd_arr)
+    # It needs to XOR every number which:
+    # -> Isn't a power of 2 / parity position
+    # -> The i th bit of the position number is = 1.
 
-# Filling codeword array
-data_i = 0
-for i in range(1, 73):
-    if not is_pow_of_2(i):
-        cd_arr[i-1] = arr[data_i]
-        data_i += 1
-        if data_i == 64:
-            break
+    for k in range(0, 7):
+        p = pow(2, k)
+        parity_bit = 0
+        for i in range(1, 73):
+            if i == p:
+                continue
+            if ((i >> k) & 1) == 1:
+                parity_bit ^= arr[i - 1]
+        arr[p - 1] = parity_bit
 
-print(cd_arr)
+    # Calculating Overall Parity:
+    overall = 0
+    for i in range(72):
+        overall ^= arr[i]
+    arr[71] = overall
 
-# Calculating parity bits
+    return arr
 
-# It needs to XOR every number which:
-# -> Isn't a power of 2 / parity position
-# -> The i th bit of the position number is = 1.
 
-for k in range(0, 7):
-    p = pow(2, k)
-    parity_bit = 0
+def data_to_codeword(arr):
+    # Generating the codeword array
+    cd_arr = np.zeros(72, dtype="u1")
+
+    # Filling codeword array
+    data_i = 0
     for i in range(1, 73):
-        if i == p:
-            continue
-        if ((i >> k) & 1) == 1:
-            parity_bit ^= cd_arr[i-1]
-    cd_arr[p-1] = parity_bit
+        if not is_pow_of_2(i):
+            cd_arr[i - 1] = arr[data_i]
+            data_i += 1
+            if data_i == 64:
+                break
+    return cd_arr
 
-print(f"Codeword Array: {cd_arr}")
 
-# Calculating Overall Parity:
-overall = 0
-for i in range(72):
-    overall ^= cd_arr[i]
-cd_arr[71] = overall
+if __name__ == "__main__":
+    # Generating the first 64 bits that will be used
+    data_arr = np.fromiter(format(secrets.randbits(64), '064b'), dtype="u1")
 
-print(f"Codeword Array Finished: \n{cd_arr}")
+    codeword = data_to_codeword(data_arr)
 
-for index in np.ndindex(cd_arr.shape):
-    print(index[0]+1, cd_arr[index])
+    codeword = calculate_parity_bits(codeword)
+
+    print(f"Codeword Array Finished: \n{codeword}")
+
+    for index in np.ndindex(codeword.shape):
+        print(index[0] + 1, codeword[index])
